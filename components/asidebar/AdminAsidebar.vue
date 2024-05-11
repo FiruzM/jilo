0e34ccd4cc5400ab3f0707ab273088d0f409621f
Firuz M <madaminovfiruz659@gmail.com>
2024-05-11 17:20:39 +0500

    feat: add products table for admin

--- components/asidebar/AdminAsidebar.vue
+++ components/asidebar/AdminAsidebar.vue
@@ -24,7 +24,7 @@ const links = [
   },
   {
     title: 'Товары',
-    url: '/admin/items',
+    url: '/admin/products',
     icon: ShoppingBasket,
   },
   {
---
+++ pages/admin/products/index.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'admin-dashboard',
+  title: 'Товары',
+})
+</script>
+
+<template>
+  <Table>
+    <TableHeader>
+      <TableRow>
+        <TableHead>
+          №
+        </TableHead>
+        <TableHead>Название</TableHead>
+        <TableHead>Количество</TableHead>
+        <TableHead class="text-right">
+          Стоимость
+        </TableHead>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      <TableRow v-for="(_, index) in 5" :key="index">
+        <TableCell class="font-medium">
+          {{ index + 1 }}
+        </TableCell>
+        <TableCell>Краситель</TableCell>
+        <TableCell>100 шт</TableCell>
+        <TableCell class="text-right">
+          25 см
+        </TableCell>
+      </TableRow>
+    </TableBody>
+  </Table>
+</template>
