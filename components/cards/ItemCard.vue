22f873ed2103f925efd4ca6a9e1923ddc06d2ee4
Firuz M <madaminovfiruz659@gmail.com>
2024-05-24 14:00:04 +0500

    fix: item card styles

--- components/cards/ItemCard.vue
+++ components/cards/ItemCard.vue
@@ -2,8 +2,8 @@
   <div class="group overflow-hidden border-white transition-all ease-in xl:rounded-3xl xl:border-[10px] xl:hover:border-[#CCE3DE] xl:hover:bg-[#CCE3DE]">
     <div class="relative flex items-center justify-center rounded-[12px] bg-[#F1F4FA] p-9 sm:rounded-3xl ">
       <img src="/assets/img/item.png" class="size-[102px] md:size-[142px] lg:size-[182px]" alt="Item">
-      <span class="py2.5 absolute left-2 top-5 rounded-md bg-primary-foreground px-1 text-xs text-[#FFDCCD] sm:left-5">-20%</span>
-      <span class="py2.5 absolute left-2 top-10 rounded-md bg-primary px-1 text-xs text-primary-foreground sm:left-5">+30 Б</span>
+      <span class="absolute left-3 top-5 rounded-[3px] bg-primary-foreground px-[6px] py-[3px] text-[7px] font-semibold leading-none text-[#FFDCCD] sm:left-5 md:rounded-md md:px-2.5 md:py-[5px] md:text-xs">-20%</span>
+      <span class="absolute left-3 top-[38px] rounded-[3px] bg-primary px-[6px] py-[3px] text-[7px] font-semibold leading-none text-primary-foreground sm:left-5 md:top-[50px] md:rounded-md md:px-2.5 md:py-[5px] md:text-xs">+30 Б</span>
 
       <svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg" class="absolute right-5 top-5 transition-all ease-in-out xl:opacity-0 xl:group-hover:opacity-100">
         <path d="M18 12C19.49 10.54 21 8.79 21 6.5C21 5.04131 20.4205 3.64236 19.3891 2.61091C18.3576 1.57946 16.9587 1 15.5 1C13.74 1 12.5 1.5 11 3C9.5 1.5 8.26 1 6.5 1C5.04131 1 3.64236 1.57946 2.61091 2.61091C1.57946 3.64236 1 5.04131 1 6.5C1 8.8 2.5 10.55 4 12L11 19L18 12Z" stroke="#4A5759" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
