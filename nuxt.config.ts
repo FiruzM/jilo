ffce57319741a8e8fee09a21242095b3df7709d3
Firuz M <madaminovfiruz659@gmail.com>
2024-08-23 10:23:11 +0500

    chore: nuxt config settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
 /* eslint-disable node/prefer-global/process */
 export default defineNuxtConfig({
 
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+     cssnano:
+       process.env.NODE_ENV === 'production'
+         ? { preset: ['default', { discardComments: { removeAll: true } }] }
+         : false, // disable cssnano when not in production
+    },
+ },
+
   modules: [
     '@vueuse/nuxt',
     '@pinia/nuxt',
